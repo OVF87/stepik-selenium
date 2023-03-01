c4f9086361ddf24b053dfe33a794e9cf3e436e72
OVF <okarskiy@gmail.com>
2023-03-01 09:15:36 +0400

    2 раздел 3 урок 6 задание

---
+++ 2_3_6.py
@@ -0,0 +1,34 @@
+# Напишите здесь свой код :-)
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    btn = browser.find_element(By.CSS_SELECTOR, "button").click()
+
+    browser.switch_to.window(browser.window_handles[1])
+
+    x_element = browser.find_element(By.CSS_SELECTOR, "#input_value")
+    x = x_element.text
+    y = calc(x)
+    print(y)
+
+    ansver = browser.find_element(By.CSS_SELECTOR, "#answer").send_keys(y)
+    btn = browser.find_element(By.CSS_SELECTOR, "button").click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
